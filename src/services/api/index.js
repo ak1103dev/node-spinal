d5898f6b1ff78416bf52ae17e95aa65b373eae93
ak110dev <ak.apichan1103@gmail.com>
2016-12-16 20:14:00 +0700

    :wrench: use morgan and winston for log

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -4,10 +4,18 @@ addPath(__dirname);
 
 const config = require('config')();
 const express = require('express');
+const morgan = require('morgan');
+const compression = require('compression');
+const winston = require('winston');
 const routes = require('./routes');
 
 const app = express();
+if (process.env.NODE_ENV === 'dev') {
+  app.use(morgan('dev'));
+} else {
+  app.use(compression());
+}
 routes(app);
 app.listen(config.api.port, () => {
-  console.log('listen on port', config.api.port);
+  winston.info('listen on port', config.api.port);
 });
--- src/services/broker/index.js
+++ src/services/broker/index.js
@@ -2,9 +2,11 @@ const { addPath } = require('app-module-path');
 addPath(`${__dirname}/../../..`);
 
 const config = require('config')();
+const winston = require('winston');
+
 const { Broker } = require('spinal');
 const broker = new Broker();
 
 broker.start(config.spinal.port, () => {
-  console.log('Spinal:Broker listening...' + config.spinal.port);
+  winston.info('Spinal:Broker listening...' + config.spinal.port);
 });
